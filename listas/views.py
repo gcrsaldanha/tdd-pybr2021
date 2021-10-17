fc96b30e31fa596a35dc9d1b9c08fb16f7865db2
Gabriel Saldanha <gabrielcrsaldanha@gmail.com>
2021-10-16 23:58:53 -0300

    Refatora view

--- listas/views.py
+++ listas/views.py
@@ -7,22 +7,13 @@ from .models import Tarefa
 def pagina_inicial(request: HttpRequest):
     if request.method == 'POST':
         texto_novo_item = request.POST['texto_novo_item']
-
         tarefa = Tarefa()
         tarefa.texto = texto_novo_item
         tarefa.save()
-        tarefas = Tarefa.objects.all()
-        return render(
-            request=request,
-            template_name='pagina_inicial.html',
-            context={'tarefas': tarefas},
-        )
 
     tarefas = Tarefa.objects.all()
     return render(
         request=request,
         template_name='pagina_inicial.html',
-        context={
-            'tarefas': tarefas,
-        }
+        context={'tarefas': tarefas}
     )
