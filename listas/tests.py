dfad6279b1b4fc8427959c77d35984409b0b7545
Gabriel Saldanha <gabrielcrsaldanha@gmail.com>
2021-10-16 23:34:00 -0300

    Cria nova tarefa com dados do POST

--- listas/tests.py
+++ listas/tests.py
@@ -1,14 +1,23 @@
 from django.test import TestCase
 from .models import Tarefa
 
+
 class TestPaginaInicial(TestCase):
     def test_pagina_inicial_renderiza_html_correto(self):
         response = self.client.get('')
         self.assertTemplateUsed(response, 'pagina_inicial.html')
 
     def test_recebe_post_request(self):
-        response = self.client.post('', {'texto_novo_item': 'Comprar livro TDD'})
+        response = self.client.post(
+            '', {'texto_novo_item': 'Comprar livro TDD'})
         self.assertIn('Comprar livro TDD', response.content.decode())
+
+    def test_salva_tarefa_no_banco_de_dados(self):
+        self.client.post('/', {'texto_novo_item': 'Comprar livro de TDD'})
+        self.assertEqual(Tarefa.objects.count(), 1)
+
+        tarefa_db = Tarefa.objects.get()
+        self.assertEqual(tarefa_db.texto, 'Comprar livro de TDD')
 
 
 class TestTarefaModel(TestCase):
--- listas/views.py
+++ listas/views.py
@@ -2,9 +2,16 @@ from django.http.request import HttpRequest
 from django.http.response import HttpResponse
 from django.shortcuts import render
 
+from .models import Tarefa
+
 def pagina_inicial(request: HttpRequest):
     if request.method == 'POST':
         texto_novo_item = request.POST['texto_novo_item']
+
+        tarefa = Tarefa()
+        tarefa.texto = texto_novo_item
+        tarefa.save()
+
         return render(
             request=request,
             template_name='pagina_inicial.html',
