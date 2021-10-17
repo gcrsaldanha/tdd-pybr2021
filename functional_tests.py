112d2cd092a5fbc2fdc0d5f083cb12350a271802
Gabriel Saldanha <gabrielcrsaldanha@gmail.com>
2021-10-16 21:21:42 -0300

    Refatora testes utilizando unittest

--- functional_tests.py
+++ functional_tests.py
@@ -1,6 +1,18 @@
+import unittest
 from selenium import webdriver
 
-browser = webdriver.Chrome()
-browser.get('http://localhost:8000')
 
-assert 'Lista de Tarefas' in browser.title, f'browser.title: {browser.title}'
+class TestPaginaInicial(unittest.TestCase):
+    def setUp(self):
+        self.browser = webdriver.Chrome()
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_pagina_inicial(self):
+        self.browser.get('http://localhost:8000')
+        self.assertIn('Lista de Tarefas', self.browser.title)
+
+
+if __name__ == '__main__':
+    unittest.main()
